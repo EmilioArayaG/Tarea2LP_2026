a7685e0a89389789caeaf6487f4c81e980967efb
emilio.araya <emiliosaraya@gmail.com>
2026-03-30 23:26:11 -0300

    Tablero

---
+++ tablero.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "tablero.h"
+#include "piezas.h"
+#include "main.h"
+
+
+Tablero* tablero_crear(int ancho, int alto){
+    Tablero *new_tablero = (Tablero*)malloc(sizeof(Tablero));
+    if (new_tablero == NULL){
+        printf("error al crear el tablero xdxdxdxd jajajaja");
+    }
+    new_tablero->W = ancho;
+    new_tablero->H = alto;
+
+    new_tablero->celdas = (void***)malloc(sizeof(void**) * alto);
+    for (int y = 0; y < alto; y++){
+        new_tablero->celdas[y] = (void**)malloc(sizeof(void*)* ancho);
+        for (int x = 0; x < ancho; x++){
+            Celda *new_celda = (Celda*)malloc(sizeof(Celda));
+            new_celda->pieza = NULL;
+            new_tablero->celdas[y][x] = new_celda;
+        }
+    }
+    return new_tablero;
+        
+}
+
+void tablero_liberar(Tablero *tablero){
+    for(int y=0; y<tablero->W; y++){
+        for(int x=0; x<tablero->H; x++){
+            Celda *celda_actual = (Celda*)tablero->celdas[y][x];
+            free(celda_actual);
+        }
+        free(tablero->celdas[y]);
+
+    }
+    free(tablero->celdas);
+}
+
+void tablero_imprimir(Juego *juego){
+    Tablero *tablero = juego->t;
+    printf("\n--- Tablero (%dx%d) ---\n", tablero->W, tablero->H);
+    for(int y = 0; y < tablero->H; y++){
+        for(int x = 0; x < tablero->W; x++){
+            Celda *c = (Celda*)tablero->celdas[y][x];
+
+            if (c->pieza == NULL){
+                printf("[ ]");
+            } else {
+                printf("[%c]", c->pieza->tipo);
+            }
+        }
+        printf("\n");
+    }
+    printf("-------------------\n");
+}
