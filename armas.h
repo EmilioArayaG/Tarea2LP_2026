58d6be4d6d186bae218d039e760f2fbc16ec4821
emilio.araya <emiliosaraya@gmail.com>
2026-03-30 22:54:01 -0300

    Inicializacion archivos

---
+++ armas.h
@@ -0,0 +1,20 @@
+#ifndef ARMAS_H
+#define ARMAS_H
+
+#include <stdbool.h>
+
+struct Juego;
+
+typedef bool (*FuncArma)(struct Juego *juego, int dir_x, int dir_y);
+
+typedef struct {
+    int municion_actual[4];
+    int municion_maxima[4];
+    FuncArma disparar[4];
+} Armas;
+
+bool escopetaa(struct Juego *juego, int dir_x, int dir_y);
+bool francotirador(struct Juego *juego, int dir_x, int dir_y);
+bool granada(struct Juego *juego, int dir_x, int dir_y);
+bool especial(struct Juego *juego, int dir_x, int dir_y);
+#endif
